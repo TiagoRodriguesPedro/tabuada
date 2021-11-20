b3bd12cdba0e928496273fa6e427ef13a387b964
Tiago Rodrigues <isatiago098@gmail.com>
2021-11-19 22:20:01 -0300

    resoluçao exercicio tabuada

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, resultado;
+           
+            Console.WriteLine(" digite um numero para cont");
+            numero = Convert.ToUInt16(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int x = 0; x <= 10; x++)
+             {
+                  resultado = numero * x;
+                  Console.WriteLine(x+" * "+numero+" = "+resultado);
+
+                Console.ReadLine();
+             }
+
+
+
+        }
+    }
+}
